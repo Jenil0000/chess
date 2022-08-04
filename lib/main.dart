7ab271f0c9cd1de3dc520f13847b768b7de69024
kuldipsavaliya <kuldipsavaliya58@gmail.com>
2022-08-04 19:34:29 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,552 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MaterialApp(home: chess()));
+}
+class chess extends StatefulWidget {
+  const chess({Key? key}) : super(key: key);
+
+  @override
+  State<chess> createState() => _chessState();
+}
+
+class _chessState extends State<chess> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Home")
+    ),
+
+
+      body: Column(children:[
+        Expanded(child: Row(children:[
+          Expanded(child: Column(children:[
+            Expanded(child: Container(
+            height: 100,
+            width: 100,
+            color: Colors.black,
+          )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+
+
+
+          ],)),
+          Expanded(child: Expanded(child: Column(children:[
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+          ],)),),
+          Expanded(child: Expanded(child: Column(children:[
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+          ],)),),
+          Expanded(child: Expanded(child: Column(children:[
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+          ],)),),
+          Expanded(child:Expanded(child: Column(children:[
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+          ],)), ),
+          Expanded(child: Expanded(child: Column(children:[
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+          ],)),),
+          Expanded(child: Expanded(child: Column(children:[
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+          ],)),),
+          Expanded(child: Expanded(child: Column(children:[
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+          ],)),),
+          Expanded(child:Expanded(child: Column(children:[
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+          ],)), ),
+          Expanded(child: Expanded(child: Column(children:[
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.white,
+            )),
+            Expanded(child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            )),
+          ],)),),
+        ],))],),
+    );
+  }
+
+}
+
+
